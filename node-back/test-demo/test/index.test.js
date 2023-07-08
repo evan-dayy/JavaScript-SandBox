f0abd8447a9062370e04f475573c8be71522c404
Evan Day <yichaoday_evan@berkeley.edu>
2023-07-08 15:42:42 -0700

    jasmine

--- node-back/test-demo/test/index.test.js
+++ node-back/test-demo/test/index.test.js
@@ -1,5 +1,39 @@
 const index = require("../index.js");
+
+
 test("test absolute path", () => {
     const result = index.absolute(-1);
     expect(result).toBe(1);
+})
+
+
+test("test absolute path - 2", () => {
+    const result = index.absolute(10);
+    expect(result).toBe(10);
+})
+
+
+test("test absolute path - 3", () => {
+    const result = index.absolute(1);
+    expect(result).toBe(1);
+})
+
+
+describe("greet", () => {
+    it("test absolute path", () => {
+        const result = index.absolute(-1);
+        expect(result).toBe(1);
+    })
+    
+    
+    it("test absolute path - 2", () => {
+        const result = index.absolute(10);
+        expect(result).toBe(10);
+    })
+    
+    
+    it("test absolute path - 3", () => {
+        const result = index.absolute(1);
+        expect(result).toBe(1);
+    })
 })
